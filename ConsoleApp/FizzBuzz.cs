406eb23ccbe56f7dc28ac50c050c4a7f809037e6
Paweł Łęczycki <pawel@msd.info.pl>
2023-12-19 13:04:33 +0100

    Czas wykonywania

--- ConsoleApp/FizzBuzz.cs
+++ ConsoleApp/FizzBuzz.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp
         public static List<string> Compute(int n)
         {
             return Enumerable.Range(1, n)
+                .AsParallel()
                 .Select(a => $"{(a % 3 == 0 ? "Fizz" : string.Empty)}{(a % 5 == 0 ? "Buzz" : string.Empty)}")
                 .Select((b, i) => string.IsNullOrEmpty(b) ? (i + 1).ToString() : b)
                 .ToList();
